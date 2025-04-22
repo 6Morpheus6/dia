24fb20fd241c6f2f6263cc4c63e89367896564e6
cocktailpeanut <cocktailpeanuts@proton.me>
2025-04-22 11:02:50 -0400

    update

--- pinokio.js
+++ pinokio.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
   version: "3.7",
   title: "Dia",
-  description: "",
+  description: "Dia is a 1.6B parameter text to speech model created by Nari Labs. Dia directly generates highly realistic dialogue from a transcript. You can condition the output on audio, enabling emotion and tone control. The model can also produce nonverbal communications like laughter, coughing, clearing throat, etc. https://github.com/nari-labs/dia",
   icon: "icon.png",
   menu: async (kernel, info) => {
     let installed = info.exists("app/env")
